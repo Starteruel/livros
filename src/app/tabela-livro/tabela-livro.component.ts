7196b66a82935e50426ed17547ed0c4c45e8ae4c
starteruel <starteruel@gmail.com>
2024-10-21 20:46:30 -0300

    Server e Bootrap

---
+++ src/app/tabela-livro/tabela-livro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-livro',
+  templateUrl: './tabela-livro.component.html',
+  styleUrl: './tabela-livro.component.css'
+})
+export class TabelaLivroComponent {
+
+}
